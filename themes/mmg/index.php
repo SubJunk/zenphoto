236beeea1af976a35dc8bd617abfc261cf16b8af
SubJunk <subjunk@gmail.com>
2025-06-03 14:44:58 +1200

    Removed old call

--- themes/mmg/index.php
+++ themes/mmg/index.php
@@ -83,7 +83,6 @@ include('includes/footer.php');
 				if (function_exists('printUserLogin_out')) {
 					?>
 					<div id="LoginContainer">
-						<?php printUserLogin_out('','',NULL,' '); ?>
 						<?php callUserFunction('printUserLogin_out', '', ''); ?>
 					</div>
 					<?php
