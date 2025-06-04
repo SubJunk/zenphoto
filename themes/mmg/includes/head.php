64aacb918f507b1836eb1a4a543a4e8d6bc00c1d
SubJunk <subjunk@gmail.com>
2025-06-04 15:12:17 +1200

    Fix not loaded js

--- themes/mmg/includes/head.php
+++ themes/mmg/includes/head.php
@@ -2,3 +2,5 @@
 <link rel="stylesheet"    href="/themes/mmg/styles/main.css"                         type="text/css">
 <link rel="stylesheet"    href='https://fonts.googleapis.com/css?family=Hind:600,400' type="text/css">
 <link rel="shortcut icon" href="/favicon.ico">
+
+<script type="text/javascript" src="<?php echo WEBPATH; ?>/themes/mmg/js/combined.js"></script>
