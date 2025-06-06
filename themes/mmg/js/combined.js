a42a8b0a2c88c5ee3f5cd15a1c150f6ae958b85b
SubJunk <subjunk@gmail.com>
2025-06-06 16:08:22 +1200

    Fix cookies not being removed

--- themes/mmg/js/combined.js
+++ themes/mmg/js/combined.js
@@ -21,9 +21,6 @@ $.fn.rating.options={cancel:'retract'};
 /* jquery.slidinglabels.js 3.2.1 */
 (function(e){e.fn.slidinglabels=function(t,n){var r={className:"form-slider",topPosition:"5px",leftPosition:"5px",axis:"x",speed:"fast"},t=e.extend(r,t),i=this.find("."+r.className+""),s=i.find("label");return s.each(function(){obj=e(this);var t=obj.parents("."+r.className+"");t.css({position:"relative"});obj.css({position:"absolute",top:r.topPosition,left:r.leftPosition,display:"inline","z-index":99});var n=e(this).next().val(),i=e(this).width(),s=i+5+"px",o=e(this).height();if(n!==""){if(r.axis=="x"){obj.stop().animate({left:"-"+s},1)}else if(r.axis=="y"){obj.stop().animate({top:"-"+o},1)}}e("input, textarea").focus(function(){var t=e(this).prev("label"),n=t.width(),i=t.height(),s=n+5+"px",o=i+"px",u=e(this).val();if(u==""){if(r.axis=="x"){t.stop().animate({left:"-"+s},r.speed)}else if(r.axis=="y"){t.stop().animate({top:"-"+o},r.speed)}}else{if(r.axis=="x"){t.css({left:"-"+s})}else if(r.axis=="y"){t.css({top:"-"+o})}}}).blur(function(){var t=e(this).prev("label"),n=e(this).val();if(n==""){if(r.axis=="x"){t.stop().animate({left:r.leftPosition},r.speed)}else if(r.axis=="y"){t.stop().animate({top:r.topPosition},r.speed)}}})})}})(jQuery);
 
-/* jQuery Cookie Plugin 1.4.1 */
-(function(e){if(typeof define==="function"&&define.amd){define(["jquery"],e)}else if(typeof exports==="object"){e(require("jquery"))}else{e(jQuery)}})(function(e){function n(e){return u.raw?e:encodeURIComponent(e)}function r(e){return u.raw?e:decodeURIComponent(e)}function i(e){return n(u.json?JSON.stringify(e):String(e))}function s(e){if(e.indexOf('"')===0){e=e.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\")}try{e=decodeURIComponent(e.replace(t," "));return u.json?JSON.parse(e):e}catch(n){}}function o(t,n){var r=u.raw?t:s(t);return e.isFunction(n)?n(r):r}var t=/\+/g;var u=e.cookie=function(t,s,a){if(s!==undefined&&!e.isFunction(s)){a=e.extend({},u.defaults,a);if(typeof a.expires==="number"){var f=a.expires,l=a.expires=new Date;l.setTime(+l+f*864e5)}return document.cookie=[n(t),"=",i(s),a.expires?"; expires="+a.expires.toUTCString():"",a.path?"; path="+a.path:"",a.domain?"; domain="+a.domain:"",a.secure?"; secure":""].join("")}var c=t?undefined:{};var h=document.cookie?document.cookie.split("; "):[];for(var p=0,d=h.length;p<d;p++){var v=h[p].split("=");var m=r(v.shift());var g=v.join("=");if(t&&t===m){c=o(g,s);break}if(!t&&(g=o(g))!==undefined){c[m]=g}}return c};u.defaults={};e.removeCookie=function(t,n){if(e.cookie(t)===undefined){return false}e.cookie(t,"",e.extend({},n,{expires:-1}));return!e.cookie(t)}});
-
 /* TipTip 1.3 from https://github.com/drewwilson/TipTip/blob/master/jquery.tipTip.minified.js */
 (function($){$.fn.tipTip=function(options){var defaults={activation:"hover",keepAlive:false,maxWidth:"200px",edgeOffset:3,defaultPosition:"bottom",delay:400,fadeIn:200,fadeOut:200,attribute:"title",content:false,enter:function(){},exit:function(){}};var opts=$.extend(defaults,options);if($("#tiptip_holder").length<=0){var tiptip_holder=$('<div id="tiptip_holder" style="max-width:'+opts.maxWidth+';"></div>');var tiptip_content=$('<div id="tiptip_content"></div>');var tiptip_arrow=$('<div id="tiptip_arrow"></div>');$("body").append(tiptip_holder.html(tiptip_content).prepend(tiptip_arrow.html('<div id="tiptip_arrow_inner"></div>')))}else{var tiptip_holder=$("#tiptip_holder");var tiptip_content=$("#tiptip_content");var tiptip_arrow=$("#tiptip_arrow")}return this.each(function(){var org_elem=$(this);if(opts.content){var org_title=opts.content}else{var org_title=org_elem.attr(opts.attribute)}if(org_title!=""){if(!opts.content){org_elem.removeAttr(opts.attribute)}var timeout=false;if(opts.activation=="hover"){org_elem.hover(function(){active_tiptip()},function(){if(!opts.keepAlive){deactive_tiptip()}});if(opts.keepAlive){tiptip_holder.hover(function(){},function(){deactive_tiptip()})}}else if(opts.activation=="focus"){org_elem.focus(function(){active_tiptip()}).blur(function(){deactive_tiptip()})}else if(opts.activation=="click"){org_elem.click(function(){active_tiptip();return false}).hover(function(){},function(){if(!opts.keepAlive){deactive_tiptip()}});if(opts.keepAlive){tiptip_holder.hover(function(){},function(){deactive_tiptip()})}}function active_tiptip(){opts.enter.call(this);tiptip_content.html(org_title);tiptip_holder.hide().removeAttr("class").css("margin","0");tiptip_arrow.removeAttr("style");var top=parseInt(org_elem.offset()['top']);var left=parseInt(org_elem.offset()['left']);var org_width=parseInt(org_elem.outerWidth());var org_height=parseInt(org_elem.outerHeight());var tip_w=tiptip_holder.outerWidth();var tip_h=tiptip_holder.outerHeight();var w_compare=Math.round((org_width-tip_w)/2);var h_compare=Math.round((org_height-tip_h)/2);var marg_left=Math.round(left+w_compare);var marg_top=Math.round(top+org_height+opts.edgeOffset);var t_class="";var arrow_top="";var arrow_left=Math.round(tip_w-12)/2;if(opts.defaultPosition=="bottom"){t_class="_bottom"}else if(opts.defaultPosition=="top"){t_class="_top"}else if(opts.defaultPosition=="left"){t_class="_left"}else if(opts.defaultPosition=="right"){t_class="_right"}var right_compare=(w_compare+left)<parseInt($(window).scrollLeft());var left_compare=(tip_w+left)>parseInt($(window).width());if((right_compare&&w_compare<0)||(t_class=="_right"&&!left_compare)||(t_class=="_left"&&left<(tip_w+opts.edgeOffset+5))){t_class="_right";arrow_top=Math.round(tip_h-13)/2;arrow_left=-12;marg_left=Math.round(left+org_width+opts.edgeOffset);marg_top=Math.round(top+h_compare)}else if((left_compare&&w_compare<0)||(t_class=="_left"&&!right_compare)){t_class="_left";arrow_top=Math.round(tip_h-13)/2;arrow_left=Math.round(tip_w);marg_left=Math.round(left-(tip_w+opts.edgeOffset+5));marg_top=Math.round(top+h_compare)}var top_compare=(top+org_height+opts.edgeOffset+tip_h+8)>parseInt($(window).height()+$(window).scrollTop());var bottom_compare=((top+org_height)-(opts.edgeOffset+tip_h+8))<0;if(top_compare||(t_class=="_bottom"&&top_compare)||(t_class=="_top"&&!bottom_compare)){if(t_class=="_top"||t_class=="_bottom"){t_class="_top"}else{t_class=t_class+"_top"}arrow_top=tip_h;marg_top=Math.round(top-(tip_h+5+opts.edgeOffset))}else if(bottom_compare|(t_class=="_top"&&bottom_compare)||(t_class=="_bottom"&&!top_compare)){if(t_class=="_top"||t_class=="_bottom"){t_class="_bottom"}else{t_class=t_class+"_bottom"}arrow_top=-12;marg_top=Math.round(top+org_height+opts.edgeOffset)}if(t_class=="_right_top"||t_class=="_left_top"){marg_top=marg_top+5}else if(t_class=="_right_bottom"||t_class=="_left_bottom"){marg_top=marg_top-5}if(t_class=="_left_top"||t_class=="_left_bottom"){marg_left=marg_left+5}tiptip_arrow.css({"margin-left":arrow_left+"px","margin-top":arrow_top+"px"});tiptip_holder.css({"margin-left":marg_left+"px","margin-top":marg_top+"px"}).attr("class","tip"+t_class);if(timeout){clearTimeout(timeout)}timeout=setTimeout(function(){tiptip_holder.stop(true,true).fadeIn(opts.fadeIn)},opts.delay)}function deactive_tiptip(){opts.exit.call(this);if(timeout){clearTimeout(timeout)}tiptip_holder.fadeOut(opts.fadeOut)}}})}})(jQuery);
 
@@ -208,37 +205,35 @@ $(function(){
 		$('#ResolutionPreferencesOptions').slideUp  ('slow', function() { });
 	});
 
-	var currentDomain = 'quadmonitorbackgrounds.com';
-
 	$(".DisableImageResizing").click(function () {
-		$.cookie('DisableImageResizing', 'yes', {expires: 30, path: '/', domain: currentDomain});
+		$.cookie('DisableImageResizing', 'yes', {expires: 30, path: '/'});
 		location.reload();
 	});
 
 	$(".EnableImageResizing").click(function () {
-		$.cookie('DisableImageResizing', null, {path: '/', domain: currentDomain});
+		$.removeCookie('DisableImageResizing', {path: '/'});
 		location.reload();
 	});
 
 	$(".ShrinkWelcomeText").click(function () {
 		$('#WelcomeTextLower').toggle();
 		$('#ExpandWelcomeTextContainer').toggle();
-		$.cookie('ShowWelcomeText', null, {path: '/', domain: currentDomain});
+		$.removeCookie('ShowWelcomeText', {path: '/'});
 	});
 
 	$(".ExpandWelcomeText").click(function () {
 		$('#WelcomeTextLower').toggle();
 		$('#ExpandWelcomeTextContainer').toggle();
-		$.cookie('ShowWelcomeText', 'yes', {expires: 30, path: '/', domain: currentDomain});
+		$.cookie('ShowWelcomeText', 'yes', {expires: 30, path: '/'});
 	});
 
 	$(".EnableNSFW").click(function () {
-		$.cookie('EnableNSFW', 'yes', {expires: 30, path: '/', domain: currentDomain});
+		$.cookie('EnableNSFW', 'yes', {expires: 30, path: '/'});
 		location.reload();
 	});
 
 	$(".DisableNSFW").click(function () {
-		$.cookie('EnableNSFW', null, {path: '/', domain: currentDomain});
+		$.removeCookie('EnableNSFW', {path: '/'});
 		location.reload();
 	});
 
